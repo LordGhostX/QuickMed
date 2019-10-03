52c7e74b6ded37c8eefb1b2cefb563ecd1abaeee
Chukslord1 <chukslord1@gmail.com>
2019-10-03 12:07:14 -0700

    setup login

--- backend/quickmed/urls.py
+++ backend/quickmed/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path("", views.index, name="index" )
-
+    path("", views.index, name="index" ),
+    path("index.html", views.index, name="index2" ),
+    path("login.html", views.login, name="login")
 ]
--- backend/quickmed/views.py
+++ backend/quickmed/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
     return render (request,'index.html')
+
+def login(request):
+    email = request.POST.get("email", None)
+    password = request.POST.get("password", None)
+
+    if email and password:
+        # Validate
+        return redirect('account/index.html')
+
+    return render (request, 'login.html')
