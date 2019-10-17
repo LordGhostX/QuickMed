6113e64618617e7249031a2a12d0bfa423b91f71
Chukslord1 <chukslord1@gmail.com>
2019-10-16 19:15:09 -0700

    added session

--- backend/quickmed/views.py
+++ backend/quickmed/views.py
@@ -6,6 +6,7 @@ from .models import UserProfile
 from .extras import get_user_history, get_billing_history
 from requests import get
 from django.http import HttpResponseRedirect
+from django.contrib.sessions.models import Session
 
 def index(request):
     return render(request, "index.html")
@@ -44,6 +45,8 @@ def login(request):
         user = auth.authenticate(username=email, password=password)
         if user is not None:
             auth.login(request, user)
+            request.session['user_id'] = user.id
+            request.session.set_test_cookie()
             return redirect("account/index.html")
         else:
             return render(request, 'login.html', {"message": "The user does not exist"})
@@ -133,12 +136,26 @@ def test_oct(request):
 
     return render(request, "account/test-oct.html", params)
 
+def process_response(self, request, response):
+        #if user and no cookie, set cookie
+        if request.user.is_authenticated() and not request.COOKIES.get('user'):
+            response.set_cookie("user", 'Hello Cookie')
+        elif not request.user.is_authenticated() and request.COOKIES.get('user'):
+            #else if if no user and cookie remove user cookie, logout
+            response.delete_cookie("user")
+        return response
+
 def logout(request):
     response = HttpResponseRedirect('../login.html')
-    response.delete_cookie('email')
-    response.delete_cookie('password')
+    try:
+        del request.session['user_id']
+        response = HttpResponseRedirect('../login.html')
+        response.delete_cookie('email')
+        response.delete_cookie('password')
+    except KeyError:
+        pass
     return response
-    
+
 
 def billing_history(request):
     params = {"history": get_billing_history("test")}
